eb5fb2d4dbfa44ded9a675db4b427384ab00ebf6
Amauri Palmeira <amauri.palmeira@outlook.com>
2023-12-11 10:16:37 -0300

    category

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,46 +4,51 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-  
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id ;
-	@JsonFormat(shape = JsonFormat.Shape.STRING , pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'" , timezone = "GMT")
-	private Instant moment ;
-	
-	@ManyToOne
-	private User client ;
-	
-	public Order() {}
+	private Long id;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant moment;
 
-	public Order(Long id, Instant moment , User client) {
+	private Integer orderStatus;
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private User client;
+
+	public Order() {
+	}
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
-		this.setClient(client) ;
+		setOrderStatus(orderStatus);
+		this.setClient(client);
 	}
-	
-	public Long getId (Long id ) {
-		return  id;
+
+	public Long getId(Long id) {
+		return id;
 	}
-	public void setId (Long id ) {
-		this.id =  id ;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public Instant getMoment() {
@@ -53,6 +58,17 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public User getClient() {
 		return client;
 	}
@@ -60,13 +76,11 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -80,7 +94,4 @@ public class Order implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
-
-	
 }
