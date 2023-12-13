0324b06e0ee35cf4aa47664fde3a9c17ca12f5fc
Amauri Palmeira <amauri.palmeira@outlook.com>
2023-12-13 20:27:13 -0300

    OrderItem, many-to-many association with extra attribute part '

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -23,11 +22,11 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
-	private Set<Product> produts = new HashSet<>();
-
+	private Set<Product> products = new HashSet<>();
+	
 	public Category() {
 	}
 
@@ -53,10 +52,10 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public Set<Product> getProduts() {
-		return produts;
+	public Set<Product> getProducts() {
+		return products;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
