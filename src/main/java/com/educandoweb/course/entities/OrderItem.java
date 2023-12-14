d1e6978ce64059397c15795bbf71701059d9443d
Amauri Palmeira <amauri.palmeira@outlook.com>
2023-12-14 17:41:42 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -44,7 +44,7 @@ public class OrderItem implements Serializable {
 	public Product getProduct() {
 		return id.getProduct();
 	}
-   @JsonIgnore
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
